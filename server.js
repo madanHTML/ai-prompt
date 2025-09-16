336e091b531a8d861d416b7b9d848a4b4209bf0c
madanHTML <madankumar14112005@gmail.com>
2025-09-16 15:06:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  const indexPath = path.join(__dirname, "index.html");
+  fs.access(indexPath, fs.constants.F_OK, (err) => {
+    if (err) {
+      res.status(404).send("index.html not found");
+    } else {
+      res.sendFile(indexPath);
+    }
+  });
+});
+
+
 // -------------------------
 // 📂 Uploads Folder
 // -------------------------
@@ -68,5 +80,6 @@ app.delete("/delete-all", (req, res) => {
 // -------------------------
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("✅ Backend running on http://localhost:" + PORT));
+
 
 
