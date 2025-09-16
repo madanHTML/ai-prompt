a63cce88091598fd6c0d5d7ec36ef63594059a1e
madanHTML <madankumar14112005@gmail.com>
2025-09-16 19:42:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const fs = require("fs");
 const app = express();
 app.use(cors());
 app.use(express.json());
+// ✅ robots.txt route
+app.get("/robots.txt", (req, res) => {
+  res.type("text/plain");
+  res.send(`User-agent: *
+Disallow:
+`);
+});
 
 app.get("/", (req, res) => {
   const indexPath = path.join(__dirname, "index.html");
@@ -94,6 +101,7 @@ app.delete("/delete-all", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => console.log(`✅ Backend running on port ${PORT}`));
+
 
 
 
